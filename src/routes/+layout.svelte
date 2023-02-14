e2559a4c0a3a10f10ae0d8dcb9d9da68dcb7fdea
Alvaro <alvarolozano2000@gmail.com>
2023-02-15 00:56:14 +0100

    Text color fix

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -12,7 +12,7 @@
 <div class="app">
 	<main class="h-full bg-black">
 		<section class="absolute bg-gradient-to-r from-blue-400 to-indigo-700 dark:from-zinc-900 dark:to-slate-900 h-full w-full grid grid-cols-12 items-center grid-flow-col">
-			<div class={`${data.error ? 'bg-red-500' : 'bg-neutral-200'} rounded-md w-full h-max md:col-start-4 md:col-span-6 col-start-2 col-span-10 p-4 flex flex-col gap-5 dark:bg-neutral-800`}>
+			<div class={`${data.error ? 'bg-red-500' : 'bg-neutral-200'} rounded-md w-full h-max md:col-start-4 md:col-span-6 col-start-2 col-span-10 p-4 flex flex-col gap-5 dark:bg-neutral-800 dark:text-white`}>
 				<slot />
 			</div>
 		</section>
